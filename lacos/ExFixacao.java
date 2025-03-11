985df11434326acad7f12ad66d78bfc101210fa5
Nathan <nathanfsj.23@gmail.com>
2025-03-11 19:12:17 -0300

    primeiros exercicios

---
+++ lacos/ExFixacao.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.exfixacao;
+
+public class ExFixacao {
+
+    public static void main(String[] args) {
+        int i, soma=0;
+        for (i = 1; i <=100;i++){
+            soma = soma + i;
+        }
+        System.out.println("Soma = "+soma);
+    }
+}
---
+++ lacos/ex2.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.exfixacao;
+
+public class ex2 {
+
+    public static void main(String[] args) {
+        int i, soma=0;
+        for (i = 6; i <=190;i++){
+            soma = soma + i;
+        }
+        System.out.println("Soma = "+soma);
+    }
+}
